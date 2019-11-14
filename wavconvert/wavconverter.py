2fb046dfd4af7040571551fd1ddac14b77c8acd8
xx0o1o0xx <bassel.almulla@gmail.com>
2019-11-13 20:25:54 -0500

    completed project

--- wavconvert/wavconverter.py
+++ wavconvert/wavconverter.py
@@ -1,11 +1,12 @@
 from pydub import AudioSegment
 import os
 import argparse
-#
-#
-# music_dir = "/Users/bassel/Google Drive/CMU/17-728/Sound Assignment/Music"
-#
-#
+import scipy.io.wavfile as wavfile
+import numpy as np
+
+music_dir = "/Users/bassel/Google Drive/CMU/17-728/Sound Assignment/Alarm"
+
+
 # formats_to_convert = ['.m4a']
 #
 # for (dirpath, dirnames, filenames) in os.walk(music_dir):
@@ -26,6 +27,21 @@ import argparse
 #             except:
 #                 print("ERROR CONVERTING " + str(filepath))
 
-for i in range(1,6):
-    for k in range(20):
-        print(str(i)+",", end="")
+# for i in range(1,6):
+#     for k in range(20):
+#         print(str(i)+",", end="")
+
+b_fs,b_y = wavfile.read(music_dir+"/alarm.wav")
+
+print(b_y.shape)
+
+one=np.array(b_y[0:1440000])
+print(one.shape)
+
+wavfile.write(music_dir+"/alarm1.wav", b_fs, one)
+start = 0
+count = 12
+while (start+1440000) < b_y.shape[0]:
+    wavfile.write(music_dir+"/alarm"+str(count)+".wav", b_fs, b_y[start:(start+1440000)])
+    start += 1440000
+    count += 1
